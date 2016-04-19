505b4fc3c07bd0f60f9b3c080c2cf055365b522d
Mike Kabischev <kabischev@gmail.com>
2016-04-19 11:21:21 +0300

    Update 10-config.sh

--- initdb/10-config.sh
+++ initdb/10-config.sh
@@ -5,7 +5,7 @@ CONF_FILE="/etc/mysql/conf.d/pinba-server.cnf"
 cat <<EOF>$CONF_FILE
 [mysqld]
 pinba_stats_gathering_period=${PINBA_STATS_GATHERING_PERIOD:-10000}
-pinba_stats_history=${PINBA_STATS_HISTORY:-900}
+pinba_stats_history=${PINBA_STATS_HISTORY:-60}
 pinba_temp_pool_size=${PINBA_TEMP_POOL_SIZE:-10000}
 pinba_request_pool_size=${PINBA_REQUEST_POOL_SIZE:-1000000}
 EOF
